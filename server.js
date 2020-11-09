c571c4585508457a9e67a125054d72d289c8e7a1
ppej1 <xodnjs1016@gmail.com>
2020-11-09 16:22:34 +0900

    Create Customer Database Tabe & Node.js Mysql connect

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,32 +8,33 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 app.get('/api/customers', (req, res) => {
-    res.send([{
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/any',
-        'name': '박태원',
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '프로그래머'
-    },
-    {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/any',
-        'name': '김영수',
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '의사 '
-    },
-    {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/any',
-        'name': '최하라',
-        'birthday': '961222',
-        'gender': '여자',
-        'job': '대학생'
-    }
-    ]);
+    connection.query(
+        "select * from customer",
+        (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+            }
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
