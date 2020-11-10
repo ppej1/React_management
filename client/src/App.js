fd8b262ea982deb20e39a9fbc9dd1b22db3c4ec2
ppej1 <xodnjs1016@gmail.com>
2020-11-10 15:27:58 +0900

    부모컴포넌트의 상태(state) 변경을 통한 고객정보 갱신

--- client/src/App.js
+++ client/src/App.js
@@ -38,9 +38,23 @@ const styles = theme => ({
 props or state => shoulComponentUpdate
  */
 class App extends Component {
-  state = {
-    customers: "",
-    completed: 0
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -87,7 +101,7 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd />
+        <CustomerAdd stateRefresh={this.stateRefresh} />
       </div>
     );
   }
